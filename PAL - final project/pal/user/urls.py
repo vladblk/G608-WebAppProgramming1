80bf98e15f417d9d82e91935345d78c8885d09c3
Vlad Blank <blankvladp@gmail.com>
2021-12-27 19:12:26 +0200

    sign in and log out functionality

--- PAL - final project/pal/user/urls.py
+++ PAL - final project/pal/user/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('sign-up/', views.signup, name='sign-up'),
+    path('sign-up/', views.sign_up, name='sign-up'),
+    path('sign-in', views.sign_in, name='sign-in'),
+    path('log-out', views.log_out, name='log-out')
 ]
--- PAL - final project/pal/user/views.py
+++ PAL - final project/pal/user/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
-def signup(request):
+def sign_up(request):
     form = UserCreationForm()
 
     if request.method == 'POST':
@@ -23,3 +25,33 @@ def signup(request):
     }
 
     return render(request, 'user/signup.html', context)
+
+
+def sign_in(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('User does not exist!')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            return redirect('posts:index')
+
+    return render(request, 'user/signin.html')
+
+
+def log_out(request):
+    if request.user.is_authenticated:
+        logout(request)
+
+        return redirect('posts:index')
+    else:
+        return redirect('user:sign-in')
